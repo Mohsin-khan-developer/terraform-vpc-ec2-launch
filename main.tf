4c07eb6744970995e491dd67c2b7f0b4d0c43a46
root <root@ip-192-168-1-247.ap-south-1.compute.internal>
2024-11-29 18:28:08 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,45 @@
+module "vpc" {
+
+  source = "../modules/vpc"
+  #  region       = var.region
+  project_name    = var.project_name
+  vpc_cidr        = var.vpc_cidr
+  pub_sub_1a_cidr = var.pub_sub_1a_cidr
+  pub_sub_1a      = var.pub_sub_1a
+  #  pub_sub_1b      = var.pub_sub_1b
+  #  pub_sub_2a      = var.pub_sub_2a
+  #  pub_sub_2b      = var.pub_sub_2b
+  #  sg-group-id  = var.sg-group-id
+
+}
+
+/* output "s-group-id" {
+
+  value = module.vpc.sg-group-id
+} */
+
+
+
+
+
+
+module "ec2" {
+
+  source = "../modules/ec2"
+
+  ami-name  = module.ec2[0].ami-name
+  inst-type = var.inst-type
+  # pub_sub_1a_cidr = var.pub_sub_1a_cidr
+  pub_sub_1a = module.vpc.pub_sub_1a_id
+  #pub_sub_1b = var.pub_sub_1b
+  #  pub_sub_2a = var.pub_sub_2a
+  #  pub_sub_2b = var.pub_sub_2b
+  s-group-id = module.vpc.sg-group-id
+
+  count = 3
+
+  /*  depends_on = [
+
+    module.vpc
+  ]*/
+}
---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "project_name" {}
+variable "region" {}
+variable "vpc_cidr" {}
+variable "access_key" {}
+variable "secret_key" {}
+variable "pub_sub_1a_cidr" {}
+variable "pub_sub_1a" {}
+#variable "pub_sub_1a_id" {}
+#variable "pub_sub_1b" {}
+#variable "pub_sub_2a" {}
+#variable "pub_sub_2b" {}
+#variable "ami-name" {}
+variable "inst-type" {}
+#variable "sg-group" {}
+
+#variable "s-group-id" {}
